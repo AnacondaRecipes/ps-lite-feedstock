17f105aaa20bce12a05eb1966488fbe42e7aad95
Scott Bailey <sbailey@anaconda.com>
2022-10-07 13:30:06 -0500

    Initial effort to add ps-lite for mxnet.

---
+++ recipe/bld.bat
@@ -0,0 +1,47 @@
+:: cmd
+echo "Building %PKG_NAME%."
+
+
+:: Isolate the build.
+mkdir Build-%PKG_NAME%
+cd Build-%PKG_NAME%
+if errorlevel 1 exit /b 1
+
+
+:: Generate the build files.
+echo "Generating the build files..."
+cmake .. %CMAKE_ARGS% ^
+      -G"Ninja" ^
+      -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+      -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      -DCMAKE_BUILD_TYPE=Release ^
+      ^
+      -DMLC_USE_CXX11=ON ^
+
+
+:: Build.
+echo "Building..."
+ninja -j%CPU_COUNT%
+if errorlevel 1 exit /b 1
+
+
+:: Perform tests.
+::  echo "Testing..."
+::  ninja test
+::  path_to\test
+::  ctest -VV --output-on-failure
+::  if errorlevel 1 exit /b 1
+
+
+:: Install.
+echo "Installing..."
+::ninja install
+:: Hand copy for now...
+copy pslite.lib %LIBRARY_LIB%\
+if errorlevel 1 exit /b 1
+xcopy /s /y /i %SRC_DIR%\include\ps %LIBRARY_INC%\
+if errorlevel 1 exit /b 1
+
+:: Error free exit.
+echo "Error free exit!"
+exit 0
